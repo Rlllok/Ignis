0629abfce31ffd1807c4bc1bd2d27f79bc511766
rlllok <kykybb29@gmail.com>
2025-04-10 15:36:00 +0300

    GLTF progress

---
+++ code/render/r_gltf.cpp
@@ -0,0 +1,343 @@
+#include "r_gltf.h"
+
+func Buffer
+AllocateBuffer(U64 size)
+{
+  Buffer result = {};
+
+  result.data = (U8*)OS_AllocateMemory(size * sizeof(U8));
+  if (result.data)
+  {
+    result.size = size;
+  }
+
+  return result;
+}
+
+func void
+FreeBuffer(Buffer* buffer)
+{
+  if (buffer->data)
+  {
+    OS_FreeMemory(buffer->data);
+  }
+
+  *buffer = {};
+}
+
+func void
+PrintBuffer(Buffer buffer)
+{
+  if (buffer.data)
+  {
+    fwrite(buffer.data, sizeof(buffer.data[0]), buffer.size, stdout);
+  }
+}
+
+func Buffer
+ReadFile(char const* file_name)
+{
+  Buffer result = {};
+
+  FILE* file = fopen(file_name, "rb");
+  if (file)
+  {
+    struct __stat64 stat;
+    __stat64(file_name, &stat);
+
+    result = AllocateBuffer(stat.st_size);
+    if (result.data)
+    {
+      if (fread(result.data, result.size, 1, file) != 1)
+      {
+        LOG_ERROR("ERROR: Unable to read \"%s\".\n", file_name);
+        FreeBuffer(&result);
+      }
+    }
+
+    fclose(file);
+  }
+  else
+  {
+    LOG_ERROR("ERROR: Unable to open \"%s\"", file_name);
+  }
+
+  return result;
+}
+
+func B32
+IsInBound(Buffer source, U64 position)
+{
+  B32 result = position < source.size;
+
+  return result;
+}
+
+func B32
+IsDigit(Buffer source, U64 position)
+{
+  B32 result = false;
+
+  if (IsInBound(source, position))
+  {
+    U8 value = source.data[position];
+
+    result = ((value >= '0') && (value <= '9'));
+  }
+
+  return result;
+}
+
+func B32
+IsWhitespace(Buffer source, U64 position)
+{
+  B32 result = false;
+
+  if (IsInBound(source, position))
+  {
+    U8 value = source.data[position];
+
+    result = ((value == ' ') || (value == '\t') || (value == '\n') || (value == '\r'));
+  }
+
+  return result;
+}
+
+func B32
+AreBuffersEqual(Buffer a, Buffer b)
+{
+  if (a.size != b.size)
+  {
+    return false;
+  }
+
+  for (U64 i = 0; i < a.size; i += 1)
+  {
+    if (a.data[i] != b.data[i])
+    {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+func U64
+FindPosition(Buffer buffer, U8 value)
+{
+  U64 result = 0;
+
+  while (buffer.data[result] != value) result += 1;
+
+  return result;
+}
+
+
+func void
+GLTFError(GLTFReader* reader, GLTFToken token, const char* message)
+{
+  reader->has_error = true;
+
+  LOG_ERROR("- LINE %d - \"%.*s\" - %s\n", reader->line_number + 1, (U32)token.value.size, (char*)token.value.data, message);
+}
+
+func GLTFToken
+GetGLTFToken(GLTFReader* reader)
+{
+  GLTFToken result = {};
+
+  Buffer source = reader->file_buffer;
+  U64 position = reader->position;
+  
+  while (IsWhitespace(source, position))
+  {
+    position += 1;
+    if (source.data[position] == '\n')
+    {
+      reader->line_number += 1;
+    }
+  }
+
+  if (IsInBound(source, position))
+  {
+    result.type = GLTF_TOKEN_TYPE_ERROR;
+    result.value.size = 1;
+    result.value.data = source.data + position;
+
+    U8 value = source.data[position];
+    position += 1;
+    
+    switch (value)
+    {
+      case ':': { result.type = GLTF_TOKEN_TYPE_COLON; } break;
+      case ',': { result.type = GLTF_TOKEN_TYPE_COMMA; } break;
+      case '{': { result.type = GLTF_TOKEN_TYPE_OPEN_BRACE; } break;
+      case '}': { result.type = GLTF_TOKEN_TYPE_CLOSE_BRACE; } break;
+      case '[': { result.type = GLTF_TOKEN_TYPE_OPEN_BRACKET; } break;
+      case ']': { result.type = GLTF_TOKEN_TYPE_CLOSE_BRACKET; } break;
+
+      case '"':
+      {
+        result.type = GLTF_TOKEN_TYPE_STRING;
+
+        U64 string_start = position;
+
+        while (IsInBound(source, position) && (source.data[position] != '"'))
+        {
+          position += 1;
+        }
+
+        result.value.data = source.data + string_start;
+        result.value.size = position - string_start;
+        
+        position += 1;
+      } break;
+
+      case '-':
+      case '0':
+      case '1':
+      case '2':
+      case '3':
+      case '4':
+      case '5':
+      case '6':
+      case '7':
+      case '8':
+      case '9':
+      {
+        U64 start = position - 1;
+
+        result.type = GLTF_TOKEN_TYPE_NUMBER;
+        
+        while (IsDigit(source, position))
+        {
+          position += 1;
+        }
+
+        if (source.data[position] == '.')
+        {
+          position += 1;
+          while (IsDigit(source, position))
+          {
+            position += 1;
+          }
+        }
+
+        result.value.data = source.data + start;
+        result.value.size = position - start;
+      } break;
+    }
+  }
+
+  reader->position = position;
+
+  return result;
+}
+
+func GLTFElement*
+ParseList(GLTFReader* reader, GLTFToken start_token, enum GLTFTokenType end_type, B32 has_labels)
+{
+  GLTFElement* first_element = 0;
+  GLTFElement* last_element = 0;
+  
+  while (!reader->has_error && IsInBound(reader->file_buffer, reader->position))
+  {
+    Buffer label = {};
+    GLTFToken current_token = GetGLTFToken(reader);
+
+    if (has_labels)
+    {
+      if (current_token.type == GLTF_TOKEN_TYPE_STRING)
+      {
+        label = current_token.value;
+
+        GLTFToken colon = GetGLTFToken(reader);
+        if (colon.type != GLTF_TOKEN_TYPE_COLON)
+        {
+          LOG_ERROR("Expect colon after field name");
+        }
+        else
+        {
+          current_token = GetGLTFToken(reader);
+        }
+      }
+    }
+
+    GLTFElement* element = ParseElement(reader, label, current_token);
+    if (element)
+    {
+      last_element = (last_element ? last_element->next_sibling : first_element) = element;
+    }
+    else if (current_token.type == end_type)
+    {
+      break;
+    }
+
+    GLTFToken comma = GetGLTFToken(reader);
+    if (comma.type == end_type)
+    {
+      break;
+    }
+    else if (comma.type != GLTF_TOKEN_TYPE_COMMA)
+    {
+      GLTFError(reader, current_token, "Unexpected token");
+    }
+  }
+
+  return first_element;
+}
+
+func GLTFElement*
+ParseElement(GLTFReader* reader, Buffer label, GLTFToken token)
+{
+  GLTFElement* result = 0;
+  GLTFElement* sub_element = 0;
+
+  if (token.type == GLTF_TOKEN_TYPE_OPEN_BRACE)
+  {
+    sub_element = ParseList(reader, token, GLTF_TOKEN_TYPE_CLOSE_BRACE, true);
+  }
+  else if (token.type == GLTF_TOKEN_TYPE_OPEN_BRACKET)
+  {
+    sub_element = ParseList(reader, token, GLTF_TOKEN_TYPE_CLOSE_BRACKET, false);
+  }
+  
+  // @TODO @ERROR: Free Memory
+  result = (GLTFElement*)OS_AllocateMemory(sizeof(GLTFElement));
+  result->label = label;
+  result->value = token.value;
+  result->first_sub_element = sub_element;
+  result->next_sibling = 0;
+
+  return result;
+}
+
+func B32
+ParseGLTF(GLTFReader* reader)
+{
+  reader->element = ParseElement(reader, {}, GetGLTFToken(reader));
+
+  return true;
+}
+
+func GLTFElement*
+LookUpElement(GLTFElement* object, Buffer label)
+{
+  GLTFElement* result = 0;
+
+  if (object)
+  {
+    for (GLTFElement* search = object->first_sub_element; search; search = search->next_sibling)
+    {
+      if (AreBuffersEqual(search->label, label))
+      {
+        result = search;
+        break;
+      }
+    }
+
+    return result;
+  }
+
+  return result;
+}
---
+++ code/render/r_gltf.h
@@ -0,0 +1,220 @@
+#pragma once
+
+#include "base/base_include.h"
+
+#include "base/base_include.cpp"
+
+struct Buffer
+{
+  U8* data;
+  U64 size;
+};
+
+#define ConstString(String) {(U8*)(String), sizeof(String) - 1}
+
+func Buffer AllocateBuffer(U64 size);
+func void FreeBuffer(Buffer* buffer);
+func void PrintBuffer(Buffer buffer);
+
+func B32 BufferIsInBound(Buffer source, U64 position);
+func B32 BufferIsDigit(Buffer source, U64 position);
+func B32 BufferIsWhitespace(Buffer source, U64 position);
+func B32 AreBuffersEqual(Buffer a, Buffer b);
+func U64 FindPosition(Buffer buffer, U8 value);
+
+enum GLTFTokenType
+{
+  GLTF_TOKEN_TYPE_ERROR,
+  
+  GLTF_TOKEN_TYPE_COLON,
+  GLTF_TOKEN_TYPE_COMMA,
+  GLTF_TOKEN_TYPE_OPEN_BRACE,
+  GLTF_TOKEN_TYPE_CLOSE_BRACE,
+  GLTF_TOKEN_TYPE_OPEN_BRACKET,
+  GLTF_TOKEN_TYPE_CLOSE_BRACKET,
+  GLTF_TOKEN_TYPE_STRING,
+  GLTF_TOKEN_TYPE_NUMBER,
+  
+  GLTF_TOKEN_TYPE_COUNT
+};
+
+struct GLTFToken
+{
+  enum GLTFTokenType type;
+  Buffer value;
+};
+
+struct GLTFElement
+{
+  Buffer label;
+  Buffer value;
+
+  GLTFElement* first_sub_element;
+  GLTFElement* next_sibling;
+};
+
+struct GLTFReader
+{
+  B32 has_error;
+  Buffer file_buffer;
+  U64 position;
+  U64 line_number;
+  GLTFElement* element;
+};
+
+func Buffer ReadGLTFFile(char const* file_name);
+func void GLTFError(GLTFReader* reader, GLTFToken token, const char* message);
+func GLTFToken GetGLTFToken(GLTFReader* reader);
+func GLTFElement* ParseList(GLTFReader* reader, GLTFToken start_token, enum GLTFTokenType end_type);
+func GLTFElement* ParseElement(GLTFReader* reader, Buffer label, GLTFToken token);
+func B32 ParseGLTF(GLTFReader* reader);
+func GLTFElement* LookUpElement(GLTFElement* object, Buffer label);
+
+struct GLTFBufferView
+{
+  U32 buffer_id;
+  U32 offset;
+  U32 byte_length;
+  U32 target;
+};
+
+#define MAX_BUFFER_VIEWS 5
+struct GLTFBufferViews
+{
+  GLTFBufferView views[MAX_BUFFER_VIEWS];
+  U32 count;
+  U32 capacity = MAX_BUFFER_VIEWS;
+};
+
+enum GLTFAttributeType
+{
+  GLTF_ATTRIBUTE_TYPE_NONE,
+  
+  GLTF_ATTRIBUTE_TYPE_POSITION,
+
+  GLTF_ATTRIBUTE_TYPE_COUNT
+};
+
+struct GLTFAttribute
+{
+  U32 acessor_id;
+  GLTFAttributeType type;
+};
+
+struct GLTFMesh
+{
+  U32 indices_accessor_id;
+  GLTFAttribute atribute;
+};
+
+struct GLTFData
+{
+  U32 deault_scene_id;
+  GLTFMesh mesh;
+  GLTFBufferViews buffer_views;
+};
+
+// Base64 Decoder
+U8 base64_map[] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P',
+                   'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f',
+                   'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
+                   'w', 'x', 'y', 'z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '+', '/'};
+
+func Buffer
+Base64Decode(Buffer in)
+{
+  U8 count = 0;
+  U8 buffer[4];
+  Buffer result = AllocateBuffer(in.size * 3 / 4);
+  U32 result_position = 0;
+
+  for (I32 i = 0; i < in.size; i += 1)
+  {
+    I32 map_index = 0;
+    while ((map_index < 64) && (base64_map[map_index] != in.data[i])) map_index += 1;
+    buffer[count] = map_index;
+    count += 1;
+
+    if (count == 4)
+    {
+      result.data[result_position] = (buffer[0] << 2) + (buffer[1] >> 4);
+      result_position += 1;
+      if (buffer[2] != 64)
+      {
+        result.data[result_position] = (buffer[1] << 4) + (buffer[2] >> 2);
+        result_position += 1;
+      }
+      if (buffer[3] != 64)
+      {
+        result.data[result_position] = (buffer[2] << 6) + buffer[3];
+        result_position += 1;
+      }
+
+      count = 0;
+    }
+  }
+
+  return result;
+}
+
+func GLTFBufferView
+GLTFGetBufferView(GLTFData* data, U32 index)
+{
+  GLTFBufferView result = {};
+
+  if (data->buffer_views.count > index)
+  {
+    result = data->buffer_views.views[index];
+  }
+  else
+  {
+    LOG_ERROR("BufferView's index is out of bound");
+  }
+  
+  return result;
+}
+
+func void
+GLTFAddBufferView(GLTFData* data, GLTFBufferView view)
+{
+  if (data->buffer_views.count == data->buffer_views.capacity)
+  {
+    LOG_ERROR("GLTFBufferViews is full");
+    return;
+  }
+
+  data->buffer_views.views[data->buffer_views.count] = view;
+  data->buffer_views.count += 1;
+}
+
+// @TODO Move
+func F64 GetNumberElement(GLTFElement* element, Buffer label)
+{
+  F64 result = 0;
+
+  GLTFElement* number = LookUpElement(element, label);
+  if (number)
+  {
+    Buffer source = number->value;
+
+    for (I32 i = 0; i < source.size; i += 1)
+    {
+      U8 number_char = source.data[i] - (U8)'0';
+
+      if (number_char < 10)
+      {
+        result = 10.0*result + (F64)number_char;
+      }
+      else
+      {
+        break;
+      }
+    }
+  }
+  else
+  {
+    LOG_ERROR("There is no element \"%.*s\"", (U32)label.size, label.data);
+  }
+
+  return result;
+}
