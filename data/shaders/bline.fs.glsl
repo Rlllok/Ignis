38d157cf98448eeca95db6b59abd7424b53489a6
rlllok <kykybb29@gmail.com>
2025-07-15 17:32:27 +0300

    Quadratic Bezier

--- data/shaders/bline.fs.glsl
+++ data/shaders/bline.fs.glsl
@@ -31,37 +31,50 @@ vec2 Bezier(vec2 a, vec2 b, vec2 c, float t)
   return mix(mix(a, c, t), mix(c, b, t), t);
 }
 
+vec2 CubicBezier(vec2 a, vec2 b, vec2 c0, vec2 c1, float t)
+{
+  vec2 ac0 = mix(a, c0, t);
+  vec2 c0c1 = mix(c0, c1, t);
+  vec2 c1b = mix(c1, b, t);
+  return mix(mix(ac0, c0c1, t), mix(c0c1, c1b, t), t);
+}
+
 void main()
 {
   int num_segs = 25;
   
   vec2 a = vec2(100.0f, 100.0f);
   vec2 b = vec2(1100.0f, 600.0f);
-  vec2 cp0 = fs_data.mouse_position;
+  vec2 c0 = fs_data.mouse_position;
+  vec2 c1 = vec2(900.0f, 200.0f);
 
   float t = (sin(fs_data.time) + 1.0f) * 0.5f;
-  vec2 a_cp0 = mix(a, cp0, t);
-  vec2 cp0_b = mix(cp0, b, t);
-  vec2 curve_p = mix(a_cp0, cp0_b, t);
+  vec2 ac0 = mix(a, c0, t);
+  vec2 c0c1 = mix(c0, c1, t);
+  vec2 c1b = mix(c1, b, t);
+  vec2 curve_p = mix(mix(ac0, c0c1, t), mix(c0c1, c1b, t), t);
 
   vec3 color = vec3(0.0f);
 
-  color += vec3(1.0f, 0.0f, 0.0f) * Point(gl_FragCoord.xy, a);
-  color += vec3(0.0f, 0.0f, 1.0f) * Point(gl_FragCoord.xy, b);
-  color += vec3(0.0f, 1.0f, 0.0f) * Point(gl_FragCoord.xy, cp0);
-  // color += vec3(0.0f, 1.0f, 1.0f) * Point(gl_FragCoord.xy, a_cp0);
-  // color += vec3(1.0f, 1.0f, 0.0f) * Point(gl_FragCoord.xy, cp0_b);
+  color += vec3(1.0f, 1.0f, 1.0f) * Point(gl_FragCoord.xy, a);
+  color += vec3(1.0f, 1.0f, 1.0f) * Point(gl_FragCoord.xy, b);
+  color += vec3(0.0f, 0.0f, 1.0f) * Point(gl_FragCoord.xy, c0);
+  color += vec3(0.0f, 0.0f, 1.0f) * Point(gl_FragCoord.xy, c1);
   color += vec3(0.2f, 0.4f, 0.6f) * Point(gl_FragCoord.xy, curve_p);
   
-  color += Line(gl_FragCoord.xy, a, cp0);
-  color += Line(gl_FragCoord.xy, cp0, b);
-  color += Line(gl_FragCoord.xy, a_cp0, cp0_b);
+  color += Line(gl_FragCoord.xy, a, c0);
+  color += Line(gl_FragCoord.xy, c0, c1);
+  color += Line(gl_FragCoord.xy, c1, b);
+  color += vec3(0.0f, 1.0f, 0.0f) * Line(gl_FragCoord.xy, ac0, c0c1);
+  color += vec3(0.0f, 1.0f, 0.0f) * Line(gl_FragCoord.xy, c0c1, c1b);
+  color += vec3(1.0f, 0.0f, 0.0f) * Line(gl_FragCoord.xy, mix(ac0, c0c1, t), mix(c0c1, c1b,t));
 
   vec2 prev_p = a;
   for (int i = 1; i < num_segs + 1; i += 1)
   {
     float t = float(i) / float(num_segs);
-    vec2 p = Bezier(a, b, cp0, t);
+    // vec2 p = Bezier(a, b, c0, t);
+    vec2 p = CubicBezier(a, b, c0, c1, t);
 
     color = max(color, vec3(0.2f, 0.4f, 0.6f) * Line(gl_FragCoord.xy, prev_p, p));
     
