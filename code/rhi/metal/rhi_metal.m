f0fa2fae42f246b493719a8827afae3a8ecffe2b
Rlllok <kykybb29@gmail.com>
2026-05-07 12:22:40 +0300

    RHI. Metal. Removed hardcoded shader

--- code/rhi/metal/rhi_metal.m
+++ code/rhi/metal/rhi_metal.m
@@ -365,48 +365,6 @@ RHI_Metal_GraphicsPipelineFromHandle(RHI_GraphicsPipeline handle) {
   return RHI_Metal_GraphicsPipelineArrayGetPointer(&_rhi_metal_context.graphics_pipelines, handle);
 }
 
-const char* rhi_metal_shader_source = R"D4LIN4R(
-using namespace metal;
-
-struct VertexIn {
-  float4 position [[attribute(0)]];
-};
-
-struct InstanceUniformData {
-  float3 translation;
-};
-
-struct IntanceDataBuffer {
-  device InstanceUniformData* uniform [[id(0)]];
-};
-
-struct VertexOut {
-  float4 position [[position]];
-  float4 color;
-};
-
-vertex VertexOut vertex_main(
-  VertexIn vertex_data [[stage_in]],
-  constant IntanceDataBuffer& instance_data_buffer [[buffer(3)]],
-  uint vid [[vertex_id]]
-) {
-  const float3 colors[3] = {
-      float3(1.0, 0.0, 0.0), // Red
-      float3(0.0, 1.0, 0.0), // Green
-      float3(0.0, 0.0, 1.0)  // Blue
-  };
-
-  VertexOut out = {0};
-  out.position = vertex_data.position + float4(instance_data_buffer.uniform->translation, 0.0f);
-  out.color = float4(colors[vid], 1.0);
-  return out;
-}
-
-fragment float4 fragment_main(VertexOut in [[stage_in]]) {
-  return in.color;
-}
-)D4LIN4R";
-
 func RHI_GraphicsPipeline
 RHI_Metal_CreateGraphicsPipeline(RHI_GraphicsPipelineCreateInfo* info) {
   @autoreleasepool {
